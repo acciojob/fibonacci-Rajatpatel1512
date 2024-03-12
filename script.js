8ab3311e46143dc20e2667094ef3249b49993cab
Rajat Patel <145240476+Rajatpatel1512@users.noreply.github.com>
2024-03-12 17:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i < num; i++) {
+      const temp = a + b;
+      a = b;
+      b = temp;
+    }
+
+    return b;
+  }
 }
 
 module.exports = fibonacci;
